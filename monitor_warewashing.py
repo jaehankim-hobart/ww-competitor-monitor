dad7ef2fb9b6b468768a74cf10a7d9709bb2a71c
jaehankim-hobart <jaehan.kim@itwfeg.com>
2026-03-04 12:27:12 -0500

    Update monitor_warewashing.py

--- monitor_warewashing.py
+++ monitor_warewashing.py
@@ -550,8 +550,7 @@ def write_preview_file(subject: str, body: str, fname: str = "preview.html"):
 </html>"""
     with open(fname, "w", encoding="utf-8") as f:
         f.write(html)
-    print(f"[TEST MODE] Wrote {fname} with rendered email HT
-
+    print(f"[TEST MODE] Wrote {fname} with rendered email HTML.")
 
 # -------------------
 # Main
