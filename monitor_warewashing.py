293efebe49ce45abb04fcb7c11b5c8059a416632
jaehankim-hobart <jaehan.kim@itwfeg.com>
2026-03-12 09:39:37 -0400

    Update monitor_warewashing.py

--- monitor_warewashing.py
+++ monitor_warewashing.py
@@ -389,7 +389,7 @@ session.headers.update({
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,"
               "image/avif,image/webp,*/*;q=0.8",
     "Accept-Language": "en-US,en;q=0.9",
-    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Encoding": "gzip, deflate",
     "Connection": "keep-alive",
     "Upgrade-Insecure-Requests": "1",
     "Sec-Fetch-Dest": "document",
